78e355462a063a446da39bd2cfa68e6a2be4ae12
William Pavei Antero <williamantero@gmail.com>
2019-06-24 16:34:07 -0300

    Adding Getter and Setter

--- src/main/java/com/scylla/demo/domain/Employee.java
+++ src/main/java/com/scylla/demo/domain/Employee.java
@@ -1,11 +1,13 @@
 package com.scylla.demo.domain;
 
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 @Table
 public class Employee {
-    @PrimaryKey
+    @PrimaryKeyColumn(ordinal = 1, type = PrimaryKeyType.PARTITIONED)
     private int id;
     private String name;
     private String address;
@@ -17,6 +19,46 @@ public class Employee {
         this.name = name;
         this.address = address;
         this.email = email;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 }
