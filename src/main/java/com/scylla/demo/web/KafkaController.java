dd74f38c1626e1f89a7d397a96e111b0e5255b9e
Allan Duarte <allan.sduarte@gmail.com>
2019-06-28 16:50:12 -0300

    refactor(kafka): Removed unnecessary constructor to instead Autowired

--- src/main/java/com/scylla/demo/web/KafkaController.java
+++ src/main/java/com/scylla/demo/web/KafkaController.java
@@ -2,19 +2,14 @@ package com.scylla.demo.web;
 
 import com.scylla.demo.kafka.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/kafka")
 public class KafkaController {
 
-    private final Producer producer;
-
     @Autowired
-    KafkaController(Producer producer) {
-        this.producer = producer;
-    }
+    private Producer producer;
 
     @PostMapping(value = "/publish")
     public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
