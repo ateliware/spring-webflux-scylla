9e1f51d51db5b6d8ff3ddfb6724bb688184999fb
Allan Duarte <allan.sduarte@gmail.com>
2019-07-04 10:53:00 -0300

    fix(app): Fix DemoApplicationTest again

--- src/test/java/com/scylla/demo/DemoApplicationTests.java
+++ src/test/java/com/scylla/demo/DemoApplicationTests.java
@@ -2,11 +2,11 @@ package com.scylla.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(DemoApplication.class)
+@WebFluxTest(DemoApplication.class)
 public class DemoApplicationTests {
 
     @Test
